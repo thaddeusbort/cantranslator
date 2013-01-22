66664a893999f6eb45ffca7aa5c1f2c6aefa4d77
Albert Bloomfield <abloomfield@gmail.com>
2013-01-22 09:53:18 -0500

    use the main Serial for logging

--- src/pic32/log.cpp
+++ src/pic32/log.cpp
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-extern HardwareSerial Serial2;
+extern HardwareSerial Serial;
 
 void debugNoNewline(const char* format, ...) {
 #ifdef __DEBUG__
@@ -13,12 +13,12 @@ void debugNoNewline(const char* format, ...) {
     char buffer[MAX_LOG_LINE_LENGTH];
     vsnprintf(buffer, MAX_LOG_LINE_LENGTH, format, args);
 
-    Serial2.print(buffer);
+    Serial.print(buffer);
 
     va_end(args);
 #endif // __DEBUG__
 }
 
 void initializeLogging() {
-    Serial2.begin(115200);
+    Serial.begin(115200);
 }
